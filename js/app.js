ce2271e197cd364349ed294f7d8472d10a0ff1d1
carlos gonzález <carlos.gonzalez@kairosds.com>
2018-04-17 16:01:01 +0200

    refactor: fuera jquery

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
-/* global jQuery */
-(function main($) {
-  $('button').on('click', (e) => {
-    switch (e.target.id) {
-      case 'leftButton':
-        if ($('#rightArea').val() !== '') {
-          $('#leftArea').val($('#rightArea').val());
-          $('#rightArea').val('');
-        }
-        break;
-      case 'rightButton':
-        if ($('#leftArea').val() !== '') {
-          $('#rightArea').val($('#leftArea').val());
-          $('#leftArea').val('');
-        }
-        break;
-      default:
-        break;
-    }
+/* global document */
+(function main(d) {
+  const leftArea = d.querySelector('#leftArea');
+  const rightArea = d.querySelector('#rightArea');
+  d.querySelectorAll('button').forEach((button) => {
+    button.addEventListener('click', () => {
+      switch (button.id) {
+        case 'leftButton':
+          if (rightArea.value !== '') {
+            leftArea.value = `${rightArea.value}\n${leftArea.value}`.trim();
+            leftArea.focus();
+            rightArea.value = '';
+          }
+          break;
+        case 'rightButton':
+          if (leftArea.value !== '') {
+            rightArea.value = `${leftArea.value}\n${rightArea.value}`.trim();
+            rightArea.focus();
+            leftArea.value = '';
+          }
+          break;
+        default: break;
+      }
+    });
   });
-}(jQuery));
+}(document));
